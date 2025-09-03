aa34ee23c0e01382eb066a20d0be520f2bd4edaf
Mark Adler <ad1m@madler.net>
2025-09-02 18:36:38 -0700

    Version 1.4

--- ptt-timer.py
+++ ptt-timer.py
@@ -1,7 +1,7 @@
 '''
   ptt-timer.py -- push-to-talk countdown timer
   Copyright (C) 2025 Mark Adler
-  Version 1.3  15 Aug 2025  Mark Adler
+  Version 1.4  2 Sep 2025  Mark Adler
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the author be held liable for any damages
@@ -35,6 +35,7 @@
 #                     Turn off the buzzer once zero is reached
 #   1.3  15 Aug 2025  Adjust times spacing on display
 #                     Add link to demonstration video to README
+#   1.4   2 Sep 2025  Use three letters for month instead of spelling it out
 
 # Provide a push-to-talk countdown timer on a Raspberry Pi with a Mini PiTFT
 # 135x240 display ($10 at https://www.adafruit.com/product/4393). Also show the
